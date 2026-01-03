da55ff8062a8b1ac7265ddbf5720f83bf9289e09
Nicollas-gthb <nicollasoliver063@gmail.com>
2026-01-02 23:00:29 -0300

    feat: Classe Connection montada

--- src/main/java/com/system/database/ConnectionFactory.java
+++ src/main/java/com/system/database/ConnectionFactory.java
@@ -1,4 +1,20 @@
 package com.system.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/library_db";
+    private static final String USER =  "postgres";
+    private static final String PASSWORD = "nickplays3";
+
+    public static Connection getConnetion(){
+        try{
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        }catch(SQLException e){
+            throw new RuntimeException("!! Erro ao conectar !!", e);
+        }
+    }
 }
